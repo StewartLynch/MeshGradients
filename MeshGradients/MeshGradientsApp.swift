0b67073cff318e8a06f335881416f50e4bd4a56f
Stewart Lynch <slynch@createchsol.com>
2024-07-10 15:05:13 -0700

    Initial Commit

---
+++ MeshGradients/MeshGradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshGradientsApp.swift
+//  MeshGradients
+//
+//  Created by Stewart Lynch on 2024-07-10.
+//
+
+import SwiftUI
+
+@main
+struct MeshGradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
